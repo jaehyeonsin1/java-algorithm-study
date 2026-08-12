7586f1eb94b45acb19fbacf9665ec5add1688ece
sinjaehyeon1 <sinjaehyeon1@naver.com>
2026-08-12 16:17:51 +0900

    solve: 배열 중복 숫자 찾기 문제 풀이

---
+++ src/day02/FindDuplicate.java
@@ -0,0 +1,41 @@
+package day02;
+
+public class FindDuplicate {
+
+    /*
+    [문제] 중복 숫자 찾기
+
+    정수 배열 numbers가 주어졌을 때,
+    같은 숫자가 두 번 이상 등장하면 true,
+    모든 숫자가 서로 다르면 false를 반환하시오.
+
+    예시 1
+    입력: [1, 2, 3, 2, 5]
+    출력: true
+
+    예시 2
+    입력: [1, 2, 3, 4, 5]
+    출력: false
+    */
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 2, 5};
+
+        boolean result = solution(numbers);
+
+        System.out.println(result); // true
+    }
+
+    public static boolean solution(int[] numbers) {
+
+        // TODO: 직접 구현
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                if(numbers[i] == numbers[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
